57e2ed7fe8f8a4d60e3f955716ce23d1842e6e98
ivanzamoraarias <faucar93@gmail.com>
2020-01-26 21:19:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+
+const app = express();
+const path = require('path');
+const fs = require("fs");
+
+var normalizedPath = path.join(__dirname, "first/js");
+
+// fs.readdirSync(normalizedPath).forEach(function(file) {
+//     require("./first/js/" + file);
+//   });
+  
+app.get('/gl-matrix.js', function (req, res) {
+    res.sendFile(path.join(__dirname+'/gl-matrix.js'));
+});
+
+app.get('/index.js', function (req, res) {
+    res.sendFile(path.join(__dirname+'/index.js'));
+});
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.get('/js/three.js', function (req, res) {
+    res.sendFile(path.join(__dirname+'/three.js'));
+});
+app.get('/js/OBJLoader.js', function (req, res) {
+    res.sendFile(path.join(__dirname+'/OBJLoader.js'));
+});
+app.get('/js/OrbitControls.js', function (req, res) {
+    res.sendFile(path.join(__dirname+'/OrbitControls.js'));
+});
+app.get('/models/arwing.obj', function (req, res) {
+    res.sendFile(path.join(__dirname+'/arwing.obj'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
